30558d9b1ef528acde332eb703add50e325a85a2
Morgan Prior <morgan@amaras-tech.co.uk>
2011-08-23 00:29:17 +0100

    Better ruby 1.9.2 and 1.8.7 compatability

--- lib/ruby_it.rb
+++ lib/ruby_it.rb
@@ -2,6 +2,11 @@ module RubyIt
   VERSION = "0.2.0"
 end
 
-require "ruby_it/document"
-require "ruby_it/ruby_it_options"
-
+#Ruby 1.9.2 did not work with the relative links
+begin
+  require_relative "ruby_it/document"
+  require_relative "ruby_it/ruby_it_options"
+rescue
+  require "ruby_it/document"
+  require "ruby_it/ruby_it_options"
+end
